2a1abb4d2a92dc0615f2a1004ded7207684b1b64
Aln732 <alexneil732@gmail.com>
2023-10-25 18:55:02 -0400

    encode()

--- main.py
+++ main.py
@@ -4,5 +4,23 @@ def encode(string):
         c = (int(i) + 3) % 10
         encodedpassword += str(c)
     return encodedpassword
-x = input()
-print(encode(x))
+
+
+def main():
+    print('''
+Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+   
+    ''')
+
+
+if __name__ == '__main.py__':
+    main()
+    men_choice = input('Please enter an option: ')
+    while True:
+        if men_choice == '1':
+            pass_to_encode = input('Password: ')
+            print(encode(pass_to_encode))
