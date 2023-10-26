1a001e1dbd5a1d253f90128902da101922558b47
Nicholas Silva <nicholasilva@Nicholass-MacBook-Pro.local>
2023-10-25 20:27:02 -0400

    ;)

--- decode.py
+++ decode.py
@@ -2,14 +2,14 @@
 def decode(password):
     decoded = ''
     for i in password:
-        if int(i)-3 == 3:
+        if int(i)-3 == -3:
             decoded += '7'
-        elif int(i)-3 == 2:
+        elif int(i)-3 == -2:
             decoded += '8'
-        elif int(i)-3 == 1:
+        elif int(i)-3 == -1:
             decoded += '9'
         else:
-            decoded += str((int(i)-3))
+            decoded += str(int(i)-3)
     return decoded
 
 
