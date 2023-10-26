31409a1cbac91c7b51cd99e750d343c19cd85591
Nicholas Silva <nicholasilva@Nicholass-MacBook-Pro.local>
2023-10-25 20:14:02 -0400

    nick decode

---
+++ decode.py
@@ -0,0 +1,15 @@
+#Nicholas Silva
+def decode(password):
+    decoded = ''
+    for i in password:
+        if int(i)-3 == 3:
+            decoded += '7'
+        elif int(i)-3 == 2:
+            decoded += '8'
+        elif int(i)-3 == 1:
+            decoded += '9'
+        else:
+            decoded += str((int(i)-3))
+    return decoded
+
+
